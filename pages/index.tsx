f9ecfb7089d181dd7dcdabbc663f8a37d9f39934
4ortytwo <arthur.burgan@spronq.com>
2021-08-27 07:11:20 +0200

    seturi and working BUTTON

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { Button, Container, Flex } from "@chakra-ui/react";
-import Head from "next/head";
-import Image from "next/image";
+import React, { useState } from "react";
+import { Button, Container, Flex, Stack, Text } from "@chakra-ui/react";
 import Polygoonz from "../artifacts/contracts/Polygoonz.sol/Polygoonz.json";
 import Web3Modal from "web3modal";
 import { ethers } from "ethers";
 import { contractAddress } from "../config";
 
 export default function Home() {
+  const [tokenURI, setTokenURI] = useState("");
   const mint = async () => {
     const web3Modal = new Web3Modal();
     const connection = await web3Modal.connect();
@@ -20,18 +19,67 @@ export default function Home() {
     );
 
     // const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
-    const transaction = await contract.purchase(2, {
-      value: ethers.utils.parseEther(`${0.1 * 2}`),
+    const transaction = await contract.purchase(1, {
+      value: ethers.utils.parseEther(`0.1`),
     });
     await transaction.wait();
+  };
+  const setBaseURI = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      contractAddress as string,
+      Polygoonz.abi,
+      signer
+    );
+
+    // const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+    const transaction = await contract.setRevealedBaseURI(
+      "ipfs://QmPrKZzh3CNKGJLCWUjus5W4iMeseF61H8dQeT6RY8KdDr/"
+    );
+    await transaction.wait();
+  };
+
+  const getTokenURI = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    // const provider = new ethers.providers.JsonRpcProvider(
+    //   process.env.NEXT_PUBLIC_API_URL
+    // );
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      contractAddress as string,
+      Polygoonz.abi,
+      signer
+    );
+
+    // const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+
+    const tokenURI = await contract.tokenURI(102);
+
+    setTokenURI(tokenURI);
+    // const response =
+    // console.log("RESPONSE: ", response);
   };
 
   return (
     <Flex justify="center">
       <Container maxW="container.xl">
-        <Button colorScheme="purple" onClick={mint}>
-          Mint
-        </Button>
+        <Stack>
+          <Button colorScheme="purple" onClick={mint}>
+            Mint
+          </Button>
+          {/* <Button colorScheme="pink" onClick={setBaseURI}>
+            Set base URI
+          </Button>
+          <Button colorScheme="pink" onClick={getTokenURI}>
+            Get Token URI
+          </Button>
+          {tokenURI && <Text>{tokenURI}</Text>} */}
+        </Stack>
       </Container>
     </Flex>
   );
