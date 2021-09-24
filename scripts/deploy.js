777efca06b320c317fff399fe887bfb9e4f08fdf
4ortytwo <arthur.burgan@spronq.com>
2021-09-24 23:27:45 +0200

    adjust deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,6 +4,9 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const { task } = require("hardhat/config");
+// import hre from "hardhat";
+// import { task } from "hardhat/config";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -12,20 +15,29 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
-
   // We get the contract to deploy
-
   // Start deployment, returning a promise that resolves to a contract object
-
+  // =======================================
   // deployed to 0x83B4c12c36E2a29990AB1c9D662B862F61C9B783
-
-  const Polygoonz = await ethers.getContractFactory("Polygoonz");
-
-  const polygoonz = await Polygoonz.deploy();
-
-  await polygoonz.deployed();
-
-  console.log("Polygoonz deployed to:", polygoonz.address);
+  // Polygoonz NFT
+  // const Polygoonz = await ethers.getContractFactory("Polygoonz");
+  // const polygoonz = await Polygoonz.deploy();
+  // await polygoonz.deployed();
+  // console.log("Polygoonz deployed to:", polygoonz.address);
+  // =======================================
+  // GOONZ Token
+  // deployed to 0x1f44AF55E91f19F4B59f84B6fda580a905455c78
+  // const Goonz = await ethers.getContractFactory("GOONZ");
+  // const goonz = await Goonz.deploy();
+  // await goonz.deployed();
+  // console.log("GOONZ Token deployed to:", goonz.address);
+  // =======================================
+  // PolygoonzFarm
+  // deployed to 0x118469B57444659b360ffF137839073b778EC639
+  // const PolygoonzFarm = await ethers.getContractFactory("PolygoonzFarm");
+  // const polygoonzFarm = await PolygoonzFarm.deploy();
+  // await polygoonzFarm.deployed();
+  // console.log("Polygoonz Farm deployed to:", polygoonzFarm.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
